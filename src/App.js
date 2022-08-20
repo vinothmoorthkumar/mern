8f34e060d446d15021bee803c4604dacd3e043c2
vinoth <vinothmoorthy.kumar@gmail.com>
2022-08-20 17:56:53 +0400

    component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import ExerciseList from "./components/exercise-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 function App() {
   return (
-    <div className="App">
-      <header className="container">
-        helloe
-      </header>
-    </div>
+    // <Router>
+    //   <div className="container">
+    //   <Navbar />
+    //   <br/>
+    //   <Route path="/" exact component={ExercisesList} />
+    //   <Route path="/edit/:id" component={EditExercise} />
+    //   <Route path="/create" component={CreateExercise} />
+    //   <Route path="/user" component={CreateUser} />
+    //   </div>
+    // </Router>
+      <Router>
+        <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ExerciseList />}/>
+          <Route path="/edit/:id" element={<EditExercise />}/>
+          <Route path="/create"  element={<CreateExercise />}/>
+          <Route path="/user"  element={<CreateUser />}/>
+        </Routes>
+        </div>
+      </Router>
   );
 }
 
